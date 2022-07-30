160df674838bc876f4f06c282d4a0f7444b35504
AxelrodAdil <taraza309@gmail.com>
2022-07-30 15:32:12 +0600

    [AM-LC] - Everyday problem solving in leetcode (30.07.22)

---
+++ src/main/java/leetcode/easy/PalindromeNumber.java
@@ -0,0 +1,17 @@
+package leetcode.easy;
+
+public class PalindromeNumber {
+
+    // https://leetcode.com/problems/palindrome-number/
+
+    private boolean isPalindrome(int x) {
+        StringBuilder s = new StringBuilder();
+        s.append(x);
+        return s.toString().equals(s.reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        System.out.println(palindromeNumber.isPalindrome(121));
+    }
+}
