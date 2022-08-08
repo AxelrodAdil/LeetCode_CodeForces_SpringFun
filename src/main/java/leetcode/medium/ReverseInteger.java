96efe235d391ccff8496f3a1d193612f54d7482c
AxelrodAdil <taraza309@gmail.com>
2022-08-08 23:21:19 +0600

    [AM-LC] - SqrtX (Easy)

---
+++ src/main/java/leetcode/medium/ReverseInteger.java
@@ -0,0 +1,20 @@
+package leetcode.medium;
+
+public class ReverseInteger {
+
+    // https://leetcode.com/problems/reverse-integer/
+
+    public int reverse(int x) {
+        int rev = 0;
+
+
+        return rev;
+    }
+
+
+
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        System.out.println(reverseInteger.reverse(-942));
+    }
+}
