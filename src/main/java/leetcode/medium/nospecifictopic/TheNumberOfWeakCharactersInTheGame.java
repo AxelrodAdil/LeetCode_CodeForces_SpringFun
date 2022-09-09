a6e7aeb481ebaba1abe9e2f6548d982a4c5fea53
AxelrodAdil <taraza309@gmail.com>
2022-09-09 15:02:18 +0600

    [AM-LC] - The Number of Weak Characters in the Game (medium)

---
+++ src/main/java/leetcode/medium/nospecifictopic/TheNumberOfWeakCharactersInTheGame.java
@@ -0,0 +1,25 @@
+package leetcode.medium.nospecifictopic;
+
+import java.util.Arrays;
+
+public class TheNumberOfWeakCharactersInTheGame {
+
+    // https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
+
+    public int numberOfWeakCharacters(int[][] properties) {
+        /*
+        Runtime: 121 ms, faster than 81.67% of Java online submissions for The Number of Weak Characters in the Game.
+        Memory Usage: 88.8 MB, less than 94.37% of Java online submissions for The Number of Weak Characters in the Game.
+         */
+        int ans = 0;
+        Arrays.sort(properties, (a, b) -> (a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]));
+        int max = Integer.MIN_VALUE;
+        for (int[] i : properties) {
+            if (i[1] < max) {
+                ans++;
+            }
+            max = Math.max(max, i[1]);
+        }
+        return ans;
+    }
+}
