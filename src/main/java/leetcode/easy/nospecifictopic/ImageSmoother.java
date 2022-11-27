7fe69c8f0759ad423c8f22f17cdc9b267d0840ca
AxelrodAdil <taraza309@gmail.com>
2022-11-27 14:51:42 +0600

    [AM-LC] - Image Smoother (easy)

---
+++ src/main/java/leetcode/easy/nospecifictopic/ImageSmoother.java
@@ -0,0 +1,40 @@
+package leetcode.easy.nospecifictopic;
+
+public class ImageSmoother {
+
+    // https://leetcode.com/problems/image-smoother/description/
+
+    private int smoothPixel(int rowIndex, int columnIndex, int rows, int columns, int[][] img) {
+        int sumOfPixelValues = 0;
+        int numberOfNeighbors = 0;
+        int modifiedRowIndex = Math.max(0, rowIndex - 1);
+        while (modifiedRowIndex <= Math.min(rows - 1, rowIndex + 1)) {
+            int modifiedColumnIndex = Math.max(0, columnIndex - 1);
+            while (modifiedColumnIndex <= Math.min(columns - 1, columnIndex + 1)) {
+                sumOfPixelValues += img[modifiedRowIndex][modifiedColumnIndex];
+                numberOfNeighbors++;
+                modifiedColumnIndex++;
+            }
+            modifiedRowIndex++;
+        }
+        return sumOfPixelValues / numberOfNeighbors;
+    }
+
+    public int[][] imageSmoother(int[][] img) {
+        /*
+        Runtime 5 ms
+        Beats 96.26%
+        Memory 42.8 MB
+        Beats 95.24%
+         */
+        int rows = img.length;
+        int columns = img[0].length;
+        int[][] smoothedImage = new int[rows][columns];
+        for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < columns; columnIndex++) {
+                smoothedImage[rowIndex][columnIndex] = smoothPixel(rowIndex, columnIndex, rows, columns, img);
+            }
+        }
+        return smoothedImage;
+    }
+}
