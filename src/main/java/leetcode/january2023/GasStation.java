00e276c280b2f483041ada9985b29e2ee7b712cb
Adil Myktybek <taraza309@gmail.com>
2023-01-12 16:23:34 +0600

    [AM-LC] - Gas Station (medium)

---
+++ src/main/java/leetcode/january2023/GasStation.java
@@ -0,0 +1,26 @@
+package leetcode.january2023;
+
+public class GasStation {
+
+    // https://leetcode.com/problems/gas-station/
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        /*
+        Runtime 2 ms
+        Beats 66.30%
+        Memory 62.6 MB
+        Beats 45.63%
+        */
+        int tank = 0, ans = 0, gasSum = 0, costSum = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                ans = i + 1;
+            }
+            gasSum += gas[i];
+            costSum += cost[i];
+        }
+        return (gasSum >= costSum) ? ans : -1;
+    }
+}
