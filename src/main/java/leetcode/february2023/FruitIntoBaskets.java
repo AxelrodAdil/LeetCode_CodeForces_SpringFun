7832112c779f1c7a2217b0d308575c0ad219eaf8
Adil Myktybek <taraza309@gmail.com>
2023-02-09 17:54:40 +0600

    [AM-LC] - Naming a Company (hard)

---
+++ src/main/java/leetcode/february2023/FruitIntoBaskets.java
@@ -0,0 +1,10 @@
+package leetcode.february2023;
+
+public class FruitIntoBaskets {
+
+    // https://leetcode.com/problems/fruit-into-baskets/
+
+//    public int totalFruit(int[] fruits) {
+//
+//    }
+}
