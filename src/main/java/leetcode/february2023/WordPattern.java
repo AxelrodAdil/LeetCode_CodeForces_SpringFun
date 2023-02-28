25e6e70c228b9f084a967d3a6e61af5230293a47
Adil Myktybek <taraza309@gmail.com>
2023-02-28 22:21:34 +0600

    [AM-LC] - Word Pattern (easy)

---
+++ src/main/java/leetcode/february2023/WordPattern.java
@@ -0,0 +1,29 @@
+package leetcode.february2023;
+
+import java.util.HashMap;
+
+public class WordPattern {
+
+    // https://leetcode.com/problems/word-pattern/
+
+    public boolean wordPattern(String pattern, String s) {
+        /*
+        Runtime 1 ms
+        Beats 90.70%
+        Memory 40.3 MB
+        Beats 54.76%
+        */
+        var hashMap = new HashMap<Character, String>();
+        var strings = s.split(" ");
+        var chars = pattern.toCharArray();
+        int i = 0;
+        if (strings.length != chars.length) return false;
+        while (i < chars.length) {
+            if (hashMap.containsKey(chars[i]) && !hashMap.get(chars[i]).equals(strings[i])) return false;
+            else if (!hashMap.containsKey(chars[i]) && hashMap.containsValue(strings[i])) return false;
+            hashMap.put(chars[i], strings[i]);
+            i++;
+        }
+        return true;
+    }
+}
