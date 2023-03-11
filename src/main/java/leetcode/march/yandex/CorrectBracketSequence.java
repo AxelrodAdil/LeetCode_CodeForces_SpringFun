6fb9f882d68b2af84d5a2987d3b2df8fe2913d43
Adil Myktybek <taraza309@gmail.com>
2023-03-11 13:04:17 +0600

    [AM-Yandex] - Correct Bracket Sequence (B)

---
+++ src/main/java/leetcode/march/yandex/CorrectBracketSequence.java
@@ -0,0 +1,31 @@
+package leetcode.march.yandex;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CorrectBracketSequence {
+
+    private static String solve(String input) {
+        var stack = new Stack<Character>();
+        for (var c : input.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return "no";
+            }
+        }
+        return stack.empty() ? "yes" : "no";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(solve(input));
+    }
+}
