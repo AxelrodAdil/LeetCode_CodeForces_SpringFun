489bc6386ee4a7f70cf13fefe3904ea52eb0e368
Adil Myktybek <taraza309@gmail.com>
2023-03-13 23:11:00 +0600

    [AM-LC] - Symmetric Tree (easy)

---
+++ src/main/java/leetcode/march2023/SymmetricTree.java
@@ -0,0 +1,42 @@
+package leetcode.march2023;
+
+public class SymmetricTree {
+
+    // https://leetcode.com/problems/symmetric-tree/
+
+    boolean isSym(TreeNodeSymmetricTree left, TreeNodeSymmetricTree right) {
+        if (left == null || right == null) return left == right;
+        if (left.val != right.val) return false;
+        return isSym(left.left, right.right) && isSym(left.right, right.left);
+    }
+
+    public boolean isSymmetric(TreeNodeSymmetricTree root) {
+        /*
+        Runtime 0 ms
+        Beats 100%
+        Memory 41 MB
+        Beats 25.47%
+        */
+        return (root == null) || isSym(root.left, root.right);
+    }
+}
+
+
+class TreeNodeSymmetricTree {
+    int val;
+    TreeNodeSymmetricTree left;
+    TreeNodeSymmetricTree right;
+
+    TreeNodeSymmetricTree() {
+    }
+
+    TreeNodeSymmetricTree(int val) {
+        this.val = val;
+    }
+
+    TreeNodeSymmetricTree(int val, TreeNodeSymmetricTree left, TreeNodeSymmetricTree right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
--- src/main/java/leetcode/march2023/yandex/GreatLinelandMigration.java
+++ src/main/java/leetcode/march2023/yandex/GreatLinelandMigration.java
@@ -1,10 +1,27 @@
 package leetcode.march2023.yandex;
 
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class GreatLinelandMigration {
 
+    private static int getFirstMinCityButTL(int[] cities, int i) {
+        return IntStream.range(i + 1, cities.length).filter(j -> cities[i] > cities[j]).findFirst().orElse(-1);
+    }
 
+    private static void solve(int[] cities, int n) {
+        var res = IntStream.range(0, n).map(i -> getFirstMinCityButTL(cities, i)).toArray();
+//        var res = IntStream.range(0, n).map(i -> getFirstMinCity(cities, i)).toArray();
+        IntStream.range(0, n).mapToObj(i -> res[i] + " ").forEachOrdered(System.out::print);
+    }
 
     public static void main(String[] args) {
-
+        //  10
+        //  1 2 3 2 1 4 2 5 3 1
+        // -1 4 3 4 -1 6 9 8 9 -1
+        Scanner input = new Scanner(System.in);
+        var n = input.nextInt();
+        var cities = IntStream.range(0, n).map(i -> input.nextInt()).toArray();
+        solve(cities, n);
     }
 }
